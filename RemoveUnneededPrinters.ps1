3ffde300fa767af3f13639dcc5fcb363cf2f5db0
D-Jeffrey <44452180+D-Jeffrey@users.noreply.github.com>
2021-07-23 21:05:34 -0600

    Update RemoveUnneededPrinters.ps1

--- RemoveUnneededPrinters.ps1
+++ RemoveUnneededPrinters.ps1
@@ -1,13 +1,8 @@
-#This Powershell script removes superfluous printers from a new Windows 10 PC.  
-#August 1, 2018
-#Josh Gold
+#This Powershell script removes the extra built in printers from Windows 10 
 
-#Forces my script to follow Powershell best practices
 Set-StrictMode -Version Latest
-# $ErrorActionPreference = 'SilentlyContinue'
 
 $PrintersToRemove = @("Microsoft XPS Document Writer", "Fax", "OneNote for Windows 10", "Send To OneNote 16")
-#$drivers = Get-PrinterDriver -Name
 
 $allprinters = Get-Printer
 
