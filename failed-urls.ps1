f866633dc2edd27f0657bcc3482dd99b1a316360
D Jeffrey <44452180+D-Jeffrey@users.noreply.github.com>
2022-07-23 13:31:35 -0600

    Add files via upload

---
+++ failed-urls.ps1
@@ -0,0 +1,54 @@
+﻿    # Test for URL Issues
+    #
+    #
+    
+       $localpath = $env:TEMP
+       $prefix = "AccessURL-"
+       $startnumber = 0
+       
+$issueURL = @(
+    "https://t.ssl.ak.dynamic.tiles.virtualearth.net/comp/ch/0213033?mkt=en-GB&it=G,LC,BX,RL&shading=hill&n=z&og=1926&cstl=rd&key=AqTrPBPn-2KPU6aSfuOMHbmetsYCgJyAd5RHBbTU8g8bHEatRUSPTqZOqWCigvXy",
+    "https://dev.virtualearth.net/webservices/v1/LoggingService/LoggingService.svc/Log?entry=0&fmt=1&type=3&group=MapControl&name=MVC&version=v8&mkt=en-CA&auth=AqTrPBPn-2KPU6aSfuOMHbmetsYCgJyAd5RHBbTU8g8bHEatRUSPTqZOqWCigvXy&jsonp=Microsoft.Maps.NetworkCallbacks.f_logCallbackRequest",
+    "https://go.microsoft.com/fwlink/?linkid=867439",
+    "https://aka.ms/mfasetup",
+    "https://az416426.vo.msecnd.net/scripts/a/ai.0.js",
+    "https://az495088.vo.msecnd.net/app-logo/customappsso_215.png",
+    "https://az495088.vo.msecnd.net/app-logo/customappsso_215.png",
+    "https://strep-prod-streaming-amsprodquickhelpcus-usct.streaming.media.azure.net/29aaae13-f622-4e32-bef8-ec67579c653a/TEAMS_Where_Are_Shared_Files_Sav.ism/manifest(format=mpd-time-csf)",
+    "https://www.youtube.com/iframe_api") 
+
+
+
+    $client = new-object System.Net.WebClient 
+
+    
+
+    
+    $issueURL | ForEach-Object {
+        $sourceuri = $_
+        $startnumber = $startnumber + 1
+        $lname = $prefix + $startnumber + ".tmp"
+             
+        $filename = $localpath + "\" + $lname
+        
+
+        write-host ("# Item #" + $startnumber + " :" + $sourceuri )
+           
+        
+        try
+        {
+            $client.DownloadFile($sourceuri, $filename)
+            write-host "Downloaded $sourceuri" -ForegroundColor Green
+            
+        }
+        catch
+        {
+            write-host "Failed $sourceuri" -ForegroundColor Yellow
+        }
+        
+       } 
+       
+       get-item -Path ($localpath + "\$prefix*.tmp") 
+ #      remove-item -Path  ($localpath + "\$prefix*.tmp") 
+
+          write-host "========= Complete"
